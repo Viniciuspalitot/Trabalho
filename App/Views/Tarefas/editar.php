d8af8b91793b86a524466c25e6ab8b26145df45c
Vinicius Palitot <viniciuspalitot10@gmail.com>
2023-07-02 23:18:48 -0300

    Revert "adicionar funcionalidade de remover filtrados"
    This reverts commit 723a839ea5778cff69888b1a9a1933c54e3f6c23.

--- App/Views/Tarefas/editar.php
+++ App/Views/Tarefas/editar.php
@@ -2,7 +2,7 @@
 <form method="post">
     Titulo: <input type="text" name="titulo" value="<?= $tarefa["titulo"]; ?>" required>
     <br/>
-    Descricao: <input type="text" name="descricao" value="<?= $tarefa["descricao"]; ?>" >
+    Descrição: <input type="text" name="descricao" value="<?= $tarefa["descricao"]; ?>" >
     <br/>
     <button type="submit">Gravar tarefa</button>
 </form>
