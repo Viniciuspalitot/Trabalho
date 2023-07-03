0069594cabcea9e427e9c3d71742560e54979d2e
Vinicius Palitot <viniciuspalitot10@gmail.com>
2023-07-02 23:20:59 -0300

    teste

--- App/Views/Tarefas/editar.php
+++ App/Views/Tarefas/editar.php
@@ -2,7 +2,7 @@
 <form method="post">
     Titulo: <input type="text" name="titulo" value="<?= $tarefa["titulo"]; ?>" required>
     <br/>
-    Descrição: <input type="text" name="descricao" value="<?= $tarefa["descricao"]; ?>" >
+    Descricaoo: <input type="text" name="descricao" value="<?= $tarefa["descricao"]; ?>" >
     <br/>
     <button type="submit">Gravar tarefa</button>
 </form>
