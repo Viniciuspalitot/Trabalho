723a839ea5778cff69888b1a9a1933c54e3f6c23
Vinicius Palitot <viniciuspalitot10@gmail.com>
2023-07-02 23:18:48 -0300

    adicionar funcionalidade de remover filtrados

--- App/Views/Tarefas/editar.php
+++ App/Views/Tarefas/editar.php
@@ -2,7 +2,7 @@
 <form method="post">
     Titulo: <input type="text" name="titulo" value="<?= $tarefa["titulo"]; ?>" required>
     <br/>
-    Descrição: <input type="text" name="descricao" value="<?= $tarefa["descricao"]; ?>" >
+    Descricao: <input type="text" name="descricao" value="<?= $tarefa["descricao"]; ?>" >
     <br/>
     <button type="submit">Gravar tarefa</button>
 </form>
